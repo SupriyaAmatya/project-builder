b407064e6fdb6d3af51539051e2fa7eb56d132ee
supriya <linkin.suppu@gmail.com>
2020-06-27 18:51:37 +0545

    Updates

---
+++ precache-manifest.105561cde1ea2fdeb37a022c0abd5c29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e721107dc6f32727aefaa0a494d2e2f6",
+    "url": "/project_builder/index.html"
+  },
+  {
+    "revision": "cdc9b168e1fbf9a51e29",
+    "url": "/project_builder/static/css/main.eb940651.chunk.css"
+  },
+  {
+    "revision": "ff7562e75bc81846aad1",
+    "url": "/project_builder/static/js/2.32bfc7d6.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/project_builder/static/js/2.32bfc7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc9b168e1fbf9a51e29",
+    "url": "/project_builder/static/js/main.d4cb6a03.chunk.js"
+  },
+  {
+    "revision": "ecccf91a42dc01c70fe7",
+    "url": "/project_builder/static/js/runtime-main.25766ee6.js"
+  }
+]);
